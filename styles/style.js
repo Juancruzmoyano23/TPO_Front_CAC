69133ecf51bb1559139cec14bd2ce4f7022bcb90
Juan Cruz Moyano <juancruzmoyanoleonangeli23@gmail.com>
2024-05-26 00:34:29 -0300

    new changue

---
+++ styles/style.js
@@ -0,0 +1,33 @@
+var cad = `}<div class="footer-column">
+<p class="parrafo1">Navegación</p>
+<ul>
+    <!--href colocar los links (alternativa para ingresar a las otras secciones)-->
+    <li><a href="" title="link 1">Home</a></li>
+    <li><a href="" title="link 2">Registro</a></li>
+    <li><a href="" title="link 3">Chef y Tips</a></li>
+    <li><a href="/pages/support.html" title="link 4">Soporte</a></li>
+    <li><a href="" title="link 5">Secciones</a></li>
+</ul>
+</div>
+<div class="footer-column">
+<p class="parrafo2">Seguinos en las redes</p>
+<ul class="social-media">
+    <!--href colocar los links-->
+    <li style="display: inline-block;"><a href="https://www.facebook.com/" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
+    <li style="display: inline-block;"><a href="https://www.instagram.com/" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
+    <li style="display: inline-block;"><a href="https://www.youtube.com/" target="_blank"><i class="fa-brands fa-youtube"></i></a></li>
+    <li style="display: inline-block;"><a href="https://www.twitter.com/" target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
+</ul>
+</div>
+<div class="footer-column">
+<ul class="columna2">
+    <li style="font-size: 12px;">© 2024 chefencasa.com</li>
+    <li style="font-size: 12px;">All Rights Reserved</li>
+    <li style="font-size: 12px;">Made by</li>
+    <li style="font-size: 14px;">Moyano, Juan</li>
+    <li style="font-size: 14px;">Álvarez, Milton</li>
+    <li style="font-size: 14px;">Galeano, Alejo</li>
+</ul>
+</div>`
+
+document.getElementById("idfooter").innerHTML = cad
